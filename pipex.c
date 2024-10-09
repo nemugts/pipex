2d3c1c17c8af81194c673cc5ec3ec9ee579ac4b5
nemu <suak1286tp@gmail.com>
2024-10-10 02:21:17 +0900

    thx

---
+++ pipex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 文字列の出力
+    printf("Hello, World!\n");
+
+    // 整数の出力
+    int num = 42;
+    printf("The number is: %d\n", num);
+
+    // 浮動小数点数の出力
+    float f = 3.14159;
+    printf("The value of pi is approximately: %.2f\n", f);
+
+    // 文字の出力
+    char ch = 'A';
+    printf("The character is: %c\n", ch);
+
+    return 0;
+    //thx
+}
